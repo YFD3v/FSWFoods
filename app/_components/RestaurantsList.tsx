c5adf3ac9db50ea1d3fa335d715953bbab12b21c
Yan Ferreira <fabioyan2@gmail.com>
2024-05-06 16:20:13 -0300

    refactor: take only 10 restaurant in restaurants list

--- app/_components/RestaurantsList.tsx
+++ app/_components/RestaurantsList.tsx
@@ -2,7 +2,7 @@ import { db } from "../_lib/prisma";
 import RestaurantItem from "./RestaurantItem";
 
 const RestaurantsList = async () => {
-  const restaurants = await db.restaurant.findMany();
+  const restaurants = await db.restaurant.findMany({ take: 10 });
   return (
     <div className="flex gap-4 overflow-x-scroll px-5 [&::-webkit-scrollbar]:hidden">
       {restaurants.map((restaurant) => (
