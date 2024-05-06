fcae943393b0c749feb7e58a1e8c7fd8af9c5ef3
Yan Ferreira <fabioyan2@gmail.com>
2024-05-06 15:04:44 -0300

    feat: add promo banner and intial categories component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import CategoryList from "./_components/CategoryList";
 import Header from "./_components/Header";
 import Search from "./_components/Search";
 
@@ -7,6 +9,20 @@ export default function Home() {
       <Header />
       <div className="px-5 pt-6">
         <Search />
+      </div>
+      <div className="px-5 pt-6">
+        <CategoryList />
+      </div>
+      <div className="px-5 pt-6">
+        <Image
+          src="/promo-banner-01.png"
+          alt="Até 30% de desconto em pizza"
+          width={0}
+          height={0}
+          className="h-auto w-full"
+          sizes="100vw"
+          quality={100}
+        />
       </div>
     </>
   );
