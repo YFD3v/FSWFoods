ece3e306748e781b5521f92452fda075851ffff4
Yan Ferreira <fabioyan2@gmail.com>
2024-05-07 16:37:52 -0300

    feat: add products page

---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,41 @@
+import { db } from "@/app/_lib/prisma";
+import { notFound } from "next/navigation";
+import ProductImage from "./_components/ProductImage";
+import ProductDetails from "./_components/ProductDetails";
+
+interface ProductPageProps {
+  //Resgatando o id da rota
+  params: {
+    id: string;
+  };
+}
+const ProductPage = async ({ params: { id } }: ProductPageProps) => {
+  const product = await db.product.findUnique({
+    where: { id },
+    include: {
+      restaurant: true,
+    },
+  });
+
+  const juices = await db.product.findMany({
+    where: {
+      category: {
+        name: "Sucos",
+      },
+    },
+    include: {
+      restaurant: true,
+    },
+  });
+
+  if (!product) return notFound();
+
+  return (
+    <div>
+      <ProductImage product={product} />
+      <ProductDetails complementaryProducts={juices} product={product} />
+    </div>
+  );
+};
+
+export default ProductPage;
