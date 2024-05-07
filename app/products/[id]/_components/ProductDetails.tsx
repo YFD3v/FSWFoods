93da0abb384ebcb83653d585082ce8f91eaae2eb
Yan Ferreira <fabioyan2@gmail.com>
2024-05-07 16:46:35 -0300

    feat: add cart button to product page

--- app/products/[id]/_components/ProductDetails.tsx
+++ app/products/[id]/_components/ProductDetails.tsx
@@ -126,9 +126,14 @@ const ProductDetails = ({
         <h3 className=" font-semibold">Sobre</h3>
         <p className="text-sm text-muted-foreground">{product.description}</p>
       </div>
+
       <div className="mt-6 space-y-3">
         <h3 className="px-5 font-semibold">Sucos</h3>
         <ProductsList products={complementaryProducts} />
+      </div>
+
+      <div className="mt-6 px-5">
+        <Button className="w-full font-semibold">Adicionar à sacola</Button>
       </div>
     </div>
   );
