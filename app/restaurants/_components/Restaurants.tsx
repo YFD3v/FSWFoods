52c93328012dce7bd8c90efc5104b3b5244cbaf8
Yan Ferreira <fabioyan2@gmail.com>
2024-05-14 16:36:01 -0300

    fix: restaurants page

--- app/restaurants/_components/Restaurants.tsx
+++ app/restaurants/_components/Restaurants.tsx
@@ -1,13 +1,17 @@
 "use client";
 
-import { Restaurant } from "@prisma/client";
+import { Restaurant, UserFavoriteRestaurant } from "@prisma/client";
 import { notFound, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { searchForRestaurants } from "../_actions/search";
 import RestaurantItem from "@/app/_components/RestaurantItem";
 import Header from "@/app/_components/Header";
 
-const Restaurants = () => {
+interface RestaurantProps {
+  userFavoriteRestaurants: UserFavoriteRestaurant[];
+}
+
+const Restaurants = ({ userFavoriteRestaurants }: RestaurantProps) => {
   const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
   const searchParams = useSearchParams();
   const searchFor = searchParams.get("searchFor");
@@ -30,6 +34,7 @@ const Restaurants = () => {
         <div className="flex w-full flex-col gap-6">
           {restaurants.map((restaurant) => (
             <RestaurantItem
+              userFavoriteRestaurants={userFavoriteRestaurants}
               key={restaurant.id}
               restaurant={restaurant}
               className="min-w-full max-w-full"
--- app/restaurants/page.tsx
+++ app/restaurants/page.tsx
@@ -1,10 +1,23 @@
 import { Suspense } from "react";
 import Restaurants from "./_components/Restaurants";
+import { authOptions } from "../_lib/auth";
+import { db } from "../_lib/prisma";
+import { getServerSession } from "next-auth";
 
-const RestaurantsPage = () => {
+const RestaurantsPage = async () => {
+  const session = await getServerSession(authOptions);
+  const userFavoriteRestaurants = await db.userFavoriteRestaurant.findMany({
+    where: {
+      userId: session?.user.id,
+    },
+    include: {
+      restaurant: true,
+    },
+  });
+
   return (
     <Suspense>
-      <Restaurants />
+      <Restaurants userFavoriteRestaurants={userFavoriteRestaurants} />
     </Suspense>
   );
 };
